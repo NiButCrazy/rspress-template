52670fbf33a359077543da8007a97f6c10ad4a31
NiButCrazy <3104955349@qq.com>
2026-04-22 11:26:16 +0800

    feat:  添加触发同步 storage 事件, 无需重新刷新页面

--- theme/index.tsx
+++ theme/index.tsx
@@ -2,8 +2,11 @@ import {
   NavHamburger as BasicNavHamburger, 
   SwitchAppearance as BasicSwitchAppearance ,
 } from '@rspress/core/theme-original';
+import './index.css';
 
 import { useDark } from '@rspress/core/runtime';
+import { useCallback } from 'react';
+
 
 // 自己的 hamburger 按钮
 function NavHamburger(){
@@ -27,15 +30,21 @@ function SwitchAppearance(){
     const preferDark = window.matchMedia('(prefers-color-scheme: dark)').matches
     // isDark 是 onclick 点击后改变的, 因此 handleClick 里的 isDark 是上一次的值
     if(preferDark === !isDark){
-      localStorage.setItem('rspress-theme-appearance', 'auto')
+      localStorage.setItem('rspress-theme-appearance', 'auto');
+      // 触发同步 storage 事件, 无需重新刷新页面
+      window.dispatchEvent(
+        new CustomEvent('RSPRESS_SYNC_STORAGE_EVENT_NAME', {
+          detail: { key: 'rspress-theme-appearance', newValue: 'auto'},
+        })
+      );
     }
   }, [isDark])
   
-  return <BasicSwitchAppearance onClick={handleClick}/>
+  return <BasicSwitchAppearance onClick={ handleClick }/>
 }
 
 
 export { NavHamburger, SwitchAppearance }
 export * from '@rspress/core/theme-original';
-import './index.css';
-import { useCallback } from 'react';
+
+
